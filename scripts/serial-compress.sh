eedff1e6e357aeb139c4198729f4e66f2505d911
Grant Wilkins <gwilkins@gpulogin1.lcrc.anl.gov>
2024-07-24 12:58:21 -0500

    add stuff

--- scripts/serial-compress.sh
+++ scripts/serial-compress.sh
@@ -7,10 +7,8 @@
 #SBATCH --time=6:00:00 
 #SBATCH --gres=gpu:1
 
-
-
 module load amd-uprof
-cd /home/gwilkins/compression-energy/src/compress_decompress
+cd /home/ac.gwilkins/compression-energy/src/compress_decompress
 mkdir -p ./serial-compress/
 make
 AMDuProfCLI timechart --event power --interval 100 --duration 99999 -o ./serial-compress ./compress_cpu
