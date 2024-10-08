7859c544af1d2e6c8c820374f517944907de4e26
grantwilkins <gfw@login1.stampede3.tacc.utexas.edu>
2024-10-07 23:23:49 -0500

    io results

--- scripts/stampede/io-test.sh
+++ scripts/stampede/io-test.sh
@@ -3,7 +3,7 @@
 #SBATCH -J io-test          # Job name
 #SBATCH -o io-test.o%j       # Name of stdout output file
 #SBATCH -e io-test.e%j       # Name of stderr error file
-#SBATCH -p spr             # Queue (partition) name
+#SBATCH -p skx             # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes 
 #SBATCH -n 1             # Total # of mpi tasks
 #SBATCH -t 6:00:00        # Run time (hh:mm:ss)
@@ -11,7 +11,6 @@
 datasets=(
     s3d/stat_planar.1.1000E-03.field.d64
     hacc/vx.f32
-    cesm/T_1_26_1800_3600.f32
 )
 
 compressors=(
